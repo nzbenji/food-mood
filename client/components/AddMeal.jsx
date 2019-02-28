1f7b58fef405d71d708d0dc9e04c80d649302f8c
Ariane Thompson <ari.r.thompson@gmail.com>
2019-03-01 11:07:16 +1300

    added input form for notes on last meal

--- client/components/AddMeal.jsx
+++ client/components/AddMeal.jsx
@@ -4,7 +4,13 @@ export class AddMeal extends Component {
   render () {
     return (
       <div>
-
+        <form>
+          <label>
+          Add meal
+            <input type="text" name="Notes" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     )
   }
