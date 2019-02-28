c1cea02b16d49f06865781d0cf266f432c2ecf0a
jess morgan <jess-morgan1@outlook.com>
2019-03-01 11:30:44 +1300

    Added db function to add new meal

--- server/db/meals.js
+++ server/db/meals.js
@@ -1,11 +1,22 @@
 const connection = require('./index')
 
-
 module.exports = {
-    userMeals
+    userMeals,
+    newMeal
   }
+
 function userMeals (db = connection) {
     return db('meals')
     .join('users', 'user_id', 'users.id')
     .where('user_id', id)
 }
+
+function newMeal (meals, db = connection) {
+    newMeal = {
+        title: meals.title,
+        time: meals.time
+    }
+    return db('meals')
+    .insert(newMeal)
+}
+
