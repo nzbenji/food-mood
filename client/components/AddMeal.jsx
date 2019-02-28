e45162c99ebb44edebf83481aaacb3d3ead0ed04
Ngapeita Matehaere Leilua-Gotz <ngapeita@outlook.com>
2019-03-01 12:40:51 +1300

    Add meal file change

--- client/components/AddMeal.jsx
+++ client/components/AddMeal.jsx
@@ -1,35 +1,96 @@
-import React, {Component} from 'react'
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+import 'date-fns';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider, TimePicker, DatePicker } from 'material-ui-pickers';
+import { Form, TextArea } from 'semantic-ui-react'
 
-export class AddMeal extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {value: ''}
+const styles = {
+    grid: {
+      width: '100%',
+    },
+  };
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-  handleChange (event) {
-    this.setState({value: event.target.value})
-  }
+class Meal extends React.Component {
+    state = {
+        // The first commit of Material-UI
+        selectedDate: new Date(),
+        notes: '',
+        meal: ''
+      };
 
-  handleSubmit (event) {
-    // this.setState({value: event.target.value})
-  }
+      handleChange = (event) => {
+        this.setState({meal: event.target.value})
+      }
+    
+      handleSubmit = (event) => {
+        this.setState({meal: event.target.value})
+      }
+    
+      handleDateChange = date => {
+        this.setState({ selectedDate: date });
+      };
 
-  render () {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <h1>Add meal</h1><br></br>
-          <h4>Add notes about your last meal</h4>
-          <label>
-            <input type="text" name="Notes" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    )
-  }
+      handleChange = event => {
+        this.setState({ notes: event.target.value });
+      }
+
+    render() {
+        console.log(this.state)
+        const { classes } = this.props;
+        const { selectedDate } = this.state;
+        return (
+            <div>
+              <form onSubmit={this.handleSubmit}>
+                <h1>Add meal</h1><br></br>
+                <label>
+                  <input type="text"/>
+                </label>
+                <input type="submit" value="Submit" />
+              </form>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Grid container className={classes.grid} alignContent="center" justify="center" >
+                <div>
+                <h3 style={{textAlign:'center', fontSize: '20px',margin:'40px', fontFamily:'Laila', letterSpacing:'4px'}}
+                >Enter a date:</h3>
+                <DatePicker style={{marginLeft: '30px'}}
+                    margin="normal"
+                    label="Date picker"
+                    value={selectedDate}
+                    onChange={this.handleDateChange}
+                />
+                
+                </div>
+
+                <Grid container className={classes.grid} alignContent="center" justify="center" ></Grid>
+                <div>
+                    <h3 style={{textAlign:'center', fontSize: '20px',margin:'40px', fontFamily:'Laila', letterSpacing:'4px'}}>Enter a time: </h3>
+                    <TimePicker style={{marginLeft: '30px'}}
+                        margin="normal"
+                        label="Time picker"
+                        value={selectedDate}
+                        onChange={this.handleDateChange}/>
+                </div>
+                </Grid>
+                <Grid container className={classes.grid} alignContent="center" justify="center" >
+                <div> 
+                    {/* <h3 style={{textAlign:'center', fontSize: '20px',margin:'40px'}}>Notes:</h3>  */}
+                      <Form style={{margin:'40px'}}>
+                            <TextArea 
+                            placeholder='Tell us more' 
+                            value={this.state.notes} 
+                            onChange={this.handleChange} 
+                            style={{width: '40rem', height: '53px', fontSize: '18px', fontFamily:'Laila', letterSpacing:'4px'}}/>
+                            <Button positive style={{height: '53px', width: '8rem', marginLeft: '18px'}}>Submit</Button>
+                        </Form>
+                </div>
+                </Grid>
+            </MuiPickersUtilsProvider>
+            </div>
+        )
+    }
 }
 
-export default AddMeal
+export default withStyles(styles)(Meal);
