d8c6443e3a2fefcea93e54550069abb0339f2620
Benjamin Peter Little <benlittle53@gmail.com>
2019-03-01 11:34:11 +1300

    Added db tests for emotions

---
+++ tests/db/dbEmotions.test.js
@@ -0,0 +1,33 @@
+const testEnv = require('./test-environment')
+const db = require('../../server/db/emotions')
+
+let testDb = null
+
+beforeEach(() => {
+  testDb = testEnv.getTestDb()
+  return testEnv.initialise(testDb)
+})
+
+afterEach(() => testEnv.cleanup(testDb))
+
+test('getEmotions returns all emotions', () => {
+  const expectedLength = 3
+  return db
+    .getEmotions()
+    .then(emotions => {
+      const actualLength = emotions.length
+      expect(actualLength).toBe(expectedLength)
+    })
+    .catch(err => expect(err).toBeNull())
+})
+
+test('emotion contain id, ranking and emoji', () => {
+  return db
+    .getEmotions()
+    .then(emotions => {
+      expect(emotions[0].id).toBeTruthy()
+      expect(emotions[0].ranking).toBeTruthy()
+      expect(emotions[0].emoji).toBeTruthy()
+    })
+    .catch(err => expect(err).toBeNull())
+})
