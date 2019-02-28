40a3dfddaf3087f5d4ab5d3a18e32fbca1008c38
Ariane Thompson <ari.r.thompson@gmail.com>
2019-03-01 11:57:36 +1300

    Fixed our file conflicts

--- server/server.js
+++ server/server.js
@@ -8,9 +8,13 @@ server.use(express.static(path.join(__dirname, './public')))
 // CHANGE ME
 const userRoutes = require('./routes/users')
 const authRoutes = require('./routes/auth')
+const emotionRoutes = require('./routes/emotions')
+const mealRoutes = require('./routes/meals')
 
 server.use('/api/v1/users', userRoutes)
 server.use('/api/v1/auth', authRoutes)
+server.use('/api/v1/emotions', emotionRoutes)
+server.use('/api/v1/meals', mealRoutes)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
