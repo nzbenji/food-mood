98eb3ccb03cc7a6ec7d8c7e17d3f35d9550fa7c9
Ngapeita Matehaere Leilua-Gotz <ngapeita@outlook.com>
2019-03-01 15:34:24 +1300

    added get all meals api functions and grouped together in moods and meals files

--- client/api/moods.js
+++ client/api/moods.js
@@ -2,6 +2,15 @@ import request from 'superagent'
 
 const url = 'http://localhost:3000/api/v1/meals/moods'
 
+export function moodMealApi (meal) {
+  request
+    .get(`${url}/${meal}`)
+    .then(res => res.body)
+    .catch(err => {
+      if (err) throw Error('Cannot get mood')
+    })
+}
+
 export function getMostRecentMoodApi (userId) {
   return request
     .get(`${url}/mostRecent/:${userId}`)
