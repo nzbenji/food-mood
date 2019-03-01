91f58ab880f10a8b6426959ee43bd179872edc72
Ngapeita Matehaere Leilua-Gotz <ngapeita@outlook.com>
2019-03-01 15:46:43 +1300

    Add mood api

--- client/api/moods.js
+++ client/api/moods.js
@@ -2,8 +2,23 @@ import request from 'superagent'
 
 const url = 'http://localhost:3000/api/v1/meals/moods'
 
+export function moodMealApi (meal) {
+  request
+    .get(`${url}/${meal}`)
+    .then(res => res.body)
+    .catch(err => {
+      if (err) throw Error('Cannot get mood')
+    })
+}
+
 export function getMostRecentMoodApi (userId) {
   return request
     .get(`${url}/mostRecent/:${userId}`)
+    .then(res => res.body)
+}
+
+export function addMoodApi (mood) {
+  return request
+    .get(`${url}/${mood}`)
     .then(res => res.body)
 }
