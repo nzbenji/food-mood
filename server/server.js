4255ea7c8f4a6a9b72866f0f7e74d3eb4a7c4bcd
jess morgan <jess-morgan1@outlook.com>
2019-03-01 14:38:03 +1300

    Added moods api route to server

--- server/server.js
+++ server/server.js
@@ -10,11 +10,13 @@ const userRoutes = require('./routes/users')
 const authRoutes = require('./routes/auth')
 const emotionRoutes = require('./routes/emotions')
 const mealRoutes = require('./routes/meals')
+const moodRoutes = require('./routes/moods')
 
 server.use('/api/v1/users', userRoutes)
 server.use('/api/v1/auth', authRoutes)
 server.use('/api/v1/emotions', emotionRoutes)
 server.use('/api/v1/meals', mealRoutes)
+server.use('/api/v1/moods', moodRoutes)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
