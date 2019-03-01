c384c92d5c66afdec6e445b095d23ce6a4e06aa2
Francine Burling-Claridge <francinebc.mail@gmail.com>
2019-03-02 11:19:46 +1300

    Added test for login route

---
+++ server/db/seeds/users.js
@@ -0,0 +1,11 @@
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {id: 1, username: 'billy', email: 'billy@hotmail.com', hash: '$argon2id$v=19$m=8,t=2,p=1$KcGPE0x6tHXN7t3CkzXgSw$ZOvCaVGpQpAl8pBllI8Z499Qb5m3L+TAMbVELlYCtt4'},
+        {id: 2, username: 'sammy', email: 'sammy@hotmail.com', hash: '$argon2id$v=19$m=8,t=2,p=1$Tt6wjQZe+zSsOX+MoYLahA$IPg5tzPLUqyik+ucDO/GFBqfaOxnBcN1OAW3LoouvUk'}
+      ])
+    })
+};
