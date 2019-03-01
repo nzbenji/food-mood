c3dc81f4b9a4bdbb2841991b5995699512977100
Ngapeita Matehaere Leilua-Gotz <ngapeita@outlook.com>
2019-03-02 12:58:44 +1300

    added class to stats

--- client/components/Stats.jsx
+++ client/components/Stats.jsx
@@ -1,3 +1,16 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
+import React, { Component } from 'react'
+
+export class Stats extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Stats
