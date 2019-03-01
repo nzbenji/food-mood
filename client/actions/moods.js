90db06cb458abdf4572c21a6bc92e7e57e747005
Francine Burling-Claridge <francinebc.mail@gmail.com>
2019-03-01 16:05:40 +1300

    Added action and reducer

--- client/actions/moods.js
+++ client/actions/moods.js
@@ -1,10 +1,34 @@
-import {addMoodApi} from '../api/moods'
+import {addMoodApi, getMostRecentMoodApi} from '../api/moods'
+
+export function getMostRecentMood (userId) {
+  return function (dispatch) {
+    dispatch(requestMood())
+    getMostRecentMoodApi(userId)
+      .then(res => {
+        const mood = res
+        dispatch(receiveMood(mood))
+      })
+  }
+}
+
+function requestMood () {
+  return {
+    type: 'REQUESTING_MOOD'
+  }
+}
+
+function receiveMood (mood) {
+  return {
+    type: 'RECEIVING_MOOD',
+    mood
+  }
+}
 
 export function saveNewMood (mood) {
   return function (dispatch) {
     // we're optimistic ;)
     dispatch(requestAddMood())
-    dispatch(mostRecentMood(mood))
+    dispatch(updateMood(mood))
     addMoodApi(mood)
       .then(() => {
         dispatch(receiveAddMood())
@@ -17,17 +41,17 @@ export function saveNewMood (mood) {
 
 function requestAddMood () {
   return {
-    type: 'REQUESTING_API'
+    type: 'REQUESTING_ADD_MOOD'
   }
 }
 
 function receiveAddMood () {
   return {
-    type: 'RECEIVING_API'
+    type: 'RECEIVING_ADD_MOOD'
   }
 }
 
-export const mostRecentMood = (mood) => {
+export const updateMood = (mood) => {
   return {
     type: 'UPDATE_MOOD',
     mood
