7e02c4ed7a8c27d083a29fb8dba724222f374572
Ngapeita Matehaere Leilua-Gotz <ngapeita@outlook.com>
2019-03-02 13:56:32 +1300

    added header in the switch

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -16,8 +16,8 @@ class App extends React.Component {
 
   render () {
     return (
-      <h1>Food mood</h1>
       <Switch>
+             <h1>Food mood</h1>
         <Route path ='/meal' component={Meal} />
         <Route path='/register' component={Register} />
         <Route path='/login' component={Login} />
