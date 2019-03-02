90c863b5cefcb624f24edb283431b672192557e5
Ngapeita Matehaere Leilua-Gotz <ngapeita@outlook.com>
2019-03-02 14:23:50 +1300

    dummy graph is now displaying

--- client/components/Stats.jsx
+++ client/components/Stats.jsx
@@ -1,11 +1,10 @@
 import React, {Component} from 'react'
-// import dummygraph from '../Images/Dummygraph.png'
 
 export class Stats extends Component {
   render () {
     return (
       <div>
-        {/* <img src={dummygraph} alt="dummygraph" /> */}
+        <img src='/images/Dummygraph.png' alt="dummygraph" />
       </div>
     )
   }
