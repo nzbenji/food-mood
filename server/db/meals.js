bdd1e34b9bfdc60b860ec3e97a54abd827e4a654
Francine Burling-Claridge <francinebc.mail@gmail.com>
2019-03-03 12:12:33 +1300

    Francines code...

--- server/db/meals.js
+++ server/db/meals.js
@@ -1,32 +1,41 @@
 const connection = require('./index')
 
 module.exports = {
-    userMeals,
-    newMeal,
-    latestMeal,
-    allMealMoods
-  }
+  userMeals,
+  newMeal,
+  latestMeal,
+  allMealMoods,
+  allUserMealsAndMoods
+}
 
 function userMeals (id, db = connection) {
-    return db('meals')
+  return db('meals')
     .where('user_id', id)
 }
 
 function newMeal (meal, db = connection) {
-    return db('meals')
+  return db('meals')
     .insert(meal)
 }
 
 function latestMeal (id, db = connection) {
-    return db('meals')
+  return db('meals')
     .where('user_id', id)
     .orderBy('time', 'desc')
     .first()
 }
 
 function allMealMoods (id, db = connection) {
-    return db('moods')
+  return db('moods')
     .join('meals', 'meals.id', 'meal_id')
     .where('moods.meal_id', id)
-    .select('moods.id', 'moods.emotion_id', 'moods.meal_id', 'moods.note', 'moods.time')
-  }
+    .select('moods.id', 'moods.emotion_id', 'moods.meal_id', 'moods.notes', 'moods.time')
+}
+
+function allUserMealsAndMoods (userId, db = connection) {
+  return db('moods')
+    .join('meals', 'meals.id', 'meal_id')
+    .where('meals.user_id', userId)
+    .select('meal_id as mealId', 'moods.id as moodId', 'emotion_id as emotionId', 'notes', 'moods.time as moodTime',
+      'meals.time as mealTime', 'title')
+}
--- server/db/seeds/moods.js
+++ server/db/seeds/moods.js
@@ -1,5 +1,4 @@
-
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('moods').del()
     .then(function () {
@@ -7,7 +6,10 @@ exports.seed = function(knex, Promise) {
       return knex('moods').insert([
         {id: 1, emotion_id: 77001, meal_id: 1, notes: 'I love this spaget!', time: '2019-03-01 14:00:45'},
         {id: 2, emotion_id: 77002, meal_id: 2, notes: 'This bread was gross', time: '2019-03-01 18:00:30'},
-        {id: 3, emotion_id: 77003, meal_id: 3, notes: 'Yummy', time: '2019-03-01 20:00:10'}
-      ]);
-    });
-};
+        {id: 3, emotion_id: 77003, meal_id: 3, notes: 'Yummy', time: '2019-03-01 20:00:10'},
+        {id: 4, emotion_id: 77001, meal_id: 1, notes: 'groos spaget', time: '2019-03-01 14:50:45'},
+        {id: 5, emotion_id: 77002, meal_id: 2, notes: 'This bread was abc', time: '2019-03-01 18:10:30'},
+        {id: 6, emotion_id: 77003, meal_id: 3, notes: 'yum', time: '2019-03-01 20:06:10'}
+      ])
+    })
+}
