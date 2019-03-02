dfd204202b3d1438533e564814eb1060fcd34439
Ngapeita Matehaere Leilua-Gotz <ngapeita@outlook.com>
2019-03-02 13:53:55 +1300

    commented out dummy graph and added header

--- client/components/Stats.jsx
+++ client/components/Stats.jsx
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
-import dummygraph from '../Images/Dummygraph.png'
+// import dummygraph from '../Images/Dummygraph.png'
 
 export class Stats extends Component {
   render () {
     return (
       <div>
-        <img src={dummygraph} alt="dummygraph" />
+        {/* <img src={dummygraph} alt="dummygraph" /> */}
       </div>
     )
   }
