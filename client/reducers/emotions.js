c0fb24ad26ddbca4b61a5a4f892d47b9622b8ebc
jess morgan <jess-morgan1@outlook.com>
2019-03-03 15:26:36 +1300

    added reducer for recenet emotion

--- client/reducers/emotions.js
+++ client/reducers/emotions.js
@@ -3,10 +3,19 @@ const initialEmotionState = []
 const emotions = (state = initialEmotionState, action) => {
   switch (action.type) {
     case 'RECEIVING_EMOTIONS':
-      return action.emotions
+      return {
+        ...state,
+        allEmotions: action.emotions
+      }
+      case 'RECEIVE_RECENT_EMOTION':
+      return {
+        ...state,
+        recentEmotion: action.recentEmoji
+      }
     default:
       return state
   }
 }
+
 
 export default emotions
