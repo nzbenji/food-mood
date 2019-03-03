c4107134d9ee717586650ddee7cffe617dbcb20e
Ngapeita Leilua - Gotz <ngapeita@outlook.com>
2019-03-04 11:48:16 +1300

    added pseudocode explaining tokens utils

--- client/utils/tokens.js
+++ client/utils/tokens.js
@@ -1,7 +1,8 @@
-// These consts handle the addition, modification or deletion of stored data items
-
+// These consts handle the addition, modification or deletion of stored token data items
+    
+// When invoked, will empty all the keys out of the local storage
 export const clearToken = () => localStorage.removeItem('ACCESS-TOKEN', null)
-
+// When passed a key name, will return that key's value
 export const getToken = () => localStorage.getItem('ACCESS-TOKEN')
-
+// When passed a key name and value, will add that key to localStorage
 export const setToken = token => localStorage.setItem('ACCESS-TOKEN', token)
