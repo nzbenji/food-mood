318f6589791ae7635b5d69bf115d3e2b8e4389ec
jess morgan <jess-morgan1@outlook.com>
2019-03-03 15:26:05 +1300

    api to get recent mood

--- client/api/moods.js
+++ client/api/moods.js
@@ -1,6 +1,7 @@
 import request from 'superagent'
 
 const url = 'http://localhost:3000/api/v1/meals/moods'
+const moodUrl = 'http://localhost:3000/api/v1/moods'
 const addMoodUrl= 'http://localhost:3000/api/v1/moods'
 
 export function moodMealApi (meal) {
@@ -14,7 +15,7 @@ export function moodMealApi (meal) {
 
 export function getMostRecentMoodApi (userId) {
   return request
-    .get(`${url}/mostRecent/:${userId}`)
+    .get(`${moodUrl}/mostRecent/${userId}`)
     .then(res => res.body)
 }
 
