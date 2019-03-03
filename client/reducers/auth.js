770033a48641a4b7ddce3781ab7ac39aa5158a74
Jessica Morgan <jess-morgan1@outlook.com>
2019-03-04 11:43:37 +1300

    removed console.log

--- client/reducers/auth.js
+++ client/reducers/auth.js
@@ -47,7 +47,6 @@ export default function authReducer (state = initialState, action) {
     case 'LOGOUT':
       clearToken()
       clearUserId()
-      console.log(getUserId())
       return {
         ...state,
         loggedIn: false,
