2bcd07dd16cc94fc8c1e051bf8487ae77dae778a
jess morgan <jess-morgan1@outlook.com>
2019-03-03 15:26:47 +1300

    function to get recent emotion

--- server/db/emotions.js
+++ server/db/emotions.js
@@ -1,9 +1,27 @@
 const connection = require('./index')
 
 module.exports = {
-  getEmotions
+  getEmotions,
+  getRecentEmotion,
+  latestEmotion
 }
 
 function getEmotions (db = connection) {
   return db('emotions')
 }
+
+function getRecentEmotion (db = connection) {
+  return db('emotions')
+    .first()
+ }
+
+ function latestEmotion (emotionId, db = connection) {
+   return db('emotions')
+   .join('moods', 'moods.emotion_id', 'emotions.id')
+   .where('emotions.id', emotionId)
+   .orderBy('moods.time', 'desc')
+   .first()
+ }
+
+
+
