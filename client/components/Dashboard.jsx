8347fe701d078d7a6a662f0dbce4a97a73854dc1
jess morgan <jess-morgan1@outlook.com>
2019-03-03 15:26:23 +1300

    showing most recent emotion/emoji

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,47 +1,71 @@
 import React from 'react'
-import {Button} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
-import '../../server/public/css/dashboard.css'
+import {getMostRecentMood} from '../actions/moods'
+import {getRecentEmotion} from '../actions/emotions'
+import { Button } from 'semantic-ui-react'
+import { withStyles } from '@material-ui/core/styles'
+import {withRouter} from 'react-router-dom'
+import {connect} from 'react-redux'
 
+const styles = {
+  grid: {
+    width: '100%',
+  },
+};
 class Dashboard extends React.Component {
+ constructor(props){
+   super(props)
+   this.state = {
+    mood: null
+   }
+  }
+
+ componentDidMount(){
+   this.props.dispatch(getMostRecentMood(this.props.userId))
+ }
+
+ componentDidUpdate(prevProps){
+  if (prevProps.currentMood !== this.props.currentMood) {
+    console.log(this.props.currentMood)
+    this.setState = {
+      mood: this.props.currentMood
+    }
+   if (this.props.currentMood.emotion_id) {
+     this.props.dispatch(getRecentEmotion(this.props.currentMood.emotion_id))
+  }
+ }
+}
+
   render () {
+    const emotions = this.props.emotions
     return (
       <div>
         <h1>Dashboard</h1>
         <div>
-          <Link to='/meal'><Button
-            textAlign="center"
-            style={{
-              width: '60rem',
-              margin: '20px',
-              height: '8rem',
-              fontSize: '3rem',
-              fontWeight: '800',
-              letterSpacing: '5px',
-              textTransform: 'uppercase',
-              display: 'flex'
-            }}
-            positive
-          >Add Meal</Button></Link>
-        </div>
-        <div>
-          <Link to='/reaction'>
-            <Button
-              style={{
-                width: '60rem',
-                margin: '20px',
-                height: '8rem',
-                fontSize: '3rem',
-                fontWeight: '800',
-                letterSpacing: '5px',
-                textTransform: 'uppercase'
-              }}
-              positive>Add Reaction</Button>
-          </Link>
+        <h3 style={{textAlign:'center', fontSize: '40px',margin:'40px', fontFamily:'Laila', letterSpacing:'4px'}}>Last Mood</h3>
+        {emotions.recentEmotion && emotions.recentEmotion.emoji ? 
+        <h3 style={{fontSize: '100px', fontFamily:'Laila', textAlign: 'center', position: 'relative', alignSelf: 'center'}}> {emotions.recentEmotion.emoji} </h3> 
+        : <div></div>}
+        <Link to='/addmeal'>
+        <Button positive style={{height: '53px', width: '8rem', position: 'relative', alignSelf: 'center'}}>Add Meal</Button>
+        </Link>
+        <br/><br/>
+        <Link to='/addreaction'>
+        <Button positive style={{height: '53px', width: '8rem', position: 'relative', alignSelf: 'center'}}>Add Reaction</Button>
+        </Link>
         </div>
       </div>
     )
   }
 }
 
-export default Dashboard
+function mapStateToProps (state) {
+  return {
+    userId: state.auth.userId,
+    loggedIn: state.auth.loggedIn,
+    currentMood: state.currentMood,
+    emotions: state.emotions
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(withStyles(styles)(Dashboard)))
