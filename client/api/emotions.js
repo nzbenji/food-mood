abae34aec9831f73693b17d84d59498b9081d0fa
jess morgan <jess-morgan1@outlook.com>
2019-03-03 15:25:46 +1300

    api for recent emotion

--- client/api/emotions.js
+++ client/api/emotions.js
@@ -2,10 +2,15 @@ import request from 'superagent'
 
 const url = 'http://localhost:3000/api/v1/emotions'
 
-const emotionGetter = () => {
+
+export const emotionGetter = () => {
   return request
     .get(`${url}/`)
     .then(res => res.body)
 }
 
-export default emotionGetter
+export const recentEmotion = (emotionId) => {
+  return request
+  .get(`${url}/mostRecent/${emotionId}`)
+  .then(res => res.body)
+}
