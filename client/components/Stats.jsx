2081b6c10bc3da6f467b1aa2953d686456217426
Benjamin Peter Little <benlittle53@gmail.com>
2019-03-04 08:44:53 +1300

    added basic bar graph

--- client/components/Stats.jsx
+++ client/components/Stats.jsx
@@ -1,15 +1,53 @@
 import React, {Component} from 'react'
 import {Redirect, withRouter} from 'react-router-dom'
 import {connect} from 'react-redux'
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  VerticalBarSeries,
+  LabelSeries
+} from 'react-vis'
 
 export class Stats extends Component {
   render () {
     if (!this.props.loggedIn) {
       return <Redirect to='/login'/>
     }
+    const data = [
+      {'y': 100, 'x': 'Mon'},
+      {'y': 112, 'x': 'Tue'},
+      {'y': 230, 'x': 'Wed'},
+      {'y': 268, 'x': 'Thu'},
+      {'y': 300, 'x': 'Fri'},
+      {'y': 310, 'x': 'Sat'},
+      {'y': 421, 'x': 'Sun'}
+    ]
+
+    const chartDomain = [0, 500]
     return (
-      <div>
-        <img src='/images/Dummygraph.png' alt="dummygraph" style={{width:'300px'}}/>
+      <div style={{paddingLeft: '80px'}}>
+
+        <XYPlot
+          xType="ordinal"
+          width={800}
+          height={500}
+          yDomain={chartDomain}
+        >
+          <XAxis />
+          <YAxis />
+          <VerticalBarSeries
+            data={data}
+          />
+          <LabelSeries
+            data={data.map(obj => {
+              return {...obj, label: obj.y.toString()}
+            })}
+            labelAnchorX="middle"
+            labelAnchorY="text-after-edge"
+          />
+        </XYPlot>
+
       </div>
     )
   }
@@ -23,3 +61,5 @@ function mapStateToProps (state) {
 }
 
 export default withRouter(connect(mapStateToProps)(Stats))
+
+{ /* <img src='/images/Dummygraph.png' alt="dummygraph" style={{width:'300px'}}/> */ }
