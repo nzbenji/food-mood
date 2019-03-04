0a48d64b60f1dd769ee6f498de17c3a1422740f5
Ngapeita Leilua - Gotz <ngapeita@outlook.com>
2019-03-05 11:42:02 +1300

    WaterInput class based component made

--- client/components/WaterInput.jsx
+++ client/components/WaterInput.jsx
@@ -1,3 +1,16 @@
 import React from 'react'
 
+ import React, { Component } from 'react'
+ 
+ export class WaterInput extends Component {
+   render() {
+     return (
+       <div>
+         
+       </div>
+     )
+   }
+ }
+ 
+ export default WaterInput
  
