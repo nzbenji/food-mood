17749a039099b21a9d7d485cb545796c7e01611d
Ngapeita Leilua - Gotz <ngapeita@outlook.com>
2019-03-05 11:57:44 +1300

    recreated route path to waterInput component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -46,6 +46,7 @@ class App extends React.Component {
             <Route path='/calendar' component={Calendar} />
             <Route path='/register' component={Register} />
             <Route path='/login' component={Login} />
+            <Route path='/water' component={WaterInput} />
           </Switch><br></br>
           {this.props.loggedIn && <NavBar />}
       </div>      
