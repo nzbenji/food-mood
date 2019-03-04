a12b5600eacb9447a4e340b2104a5d3eb5a101c5
Ngapeita Leilua - Gotz <ngapeita@outlook.com>
2019-03-04 16:14:19 +1300

    All tests for this reducer have passed

--- tests/client/reducers/isLoading.test.js
+++ tests/client/reducers/isLoading.test.js
@@ -29,3 +29,48 @@ test('Is loading requesting emotions state', () => {
     const actualLoading = reducer.default('', loading)
     expect(actualLoading.state).toBe(expectedLoading.state)
   })
+
+  // REQUESTING_ADD_MOOD
+test('Is loading requesting emotions state', () => {
+    const loading = {
+      type: 'REQUESTING_ADD_MOOD',
+      return: true
+    }
+  
+    const expectedLoading = {
+    return: true
+    }
+  
+    const actualLoading = reducer.default('', loading)
+    expect(actualLoading.state).toBe(expectedLoading.state)
+  })
+
+  // RECEIVING_ADD_MOOD
+test('Is loading requesting emotions state', () => {
+    const loading = {
+      type: 'RECEIVING_ADD_MOOD',
+      return: false
+    }
+  
+    const expectedLoading = {
+    return: false
+    }
+  
+    const actualLoading = reducer.default('', loading)
+    expect(actualLoading.state).toBe(expectedLoading.state)
+  })
+
+   // REQUESTING_MOOD
+test('Is loading requesting emotions state', () => {
+    const loading = {
+      type: 'REQUESTING_MOOD',
+      return: true
+    }
+  
+    const expectedLoading = {
+    return: true
+    }
+  
+    const actualLoading = reducer.default('', loading)
+    expect(actualLoading.state).toBe(expectedLoading.state)
+  })
