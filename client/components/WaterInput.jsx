8273a69c907483d72ed52b47cdb945823d67331c
Ngapeita Leilua - Gotz <ngapeita@outlook.com>
2019-03-05 12:55:39 +1300

    added a space between pseudocode and class

--- client/components/WaterInput.jsx
+++ client/components/WaterInput.jsx
@@ -5,6 +5,7 @@ const convert = require('convert-units')
  
  // Write a component that calculates how much water you should drink on a daily basis 
  // We need an input for height, weight and age to do this
+ 
  export class WaterInput extends Component {
    render() {
      return (
