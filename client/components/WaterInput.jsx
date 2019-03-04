cc7a55f76a0fa3e5d7cf5694ca2f7a8c818c61c7
Ngapeita Leilua - Gotz <ngapeita@outlook.com>
2019-03-05 12:56:12 +1300

    deleted data

--- client/components/WaterInput.jsx
+++ client/components/WaterInput.jsx
@@ -5,14 +5,13 @@ const convert = require('convert-units')
  
  // Write a component that calculates how much water you should drink on a daily basis 
  // We need an input for height, weight and age to do this
- 
+
  export class WaterInput extends Component {
    render() {
      return (
        <div>
            <center>
          <h1>Water intake</h1>
-         {/* <h4>convert(68).from('kg').to('lb')</h4> */}
          </center>
        </div>
      )
