3c8e078aee61d23347fb36e495a9dc4a4bf2e442
Ariane Thompson <ari.r.thompson@gmail.com>
2019-03-05 10:47:09 +1300

    moved button to correct place

--- client/components/Meal.jsx
+++ client/components/Meal.jsx
@@ -26,12 +26,6 @@ class Meal extends React.Component {
               <li key={mood.id} style={{fontSize: '40px', listStyle: 'none', margin: '40px'}}>
                 {`${this.props.emotions.find(emotion => emotion.id === mood.emotionId).emoji} ${mood.time.slice(11, 16)} ${mood.notes}`}
                 <Link to={{
-                  pathname: `/editmeal`,
-                  state: {meal: meal}}}>
-                  <button positive style={{height: '53px', width: '8rem', position: 'relative', alignSelf: 'center', backgroundColor: '#0ba8bc'}}>
-                  Edit Meal</button>
-                </Link>
-                <Link to={{
                   pathname: `/editmood`,
                   state: {mood: mood, meal: meal}}}>
                   <button positive style={{height: '53px', width: '8rem', position: 'relative', alignSelf: 'center', backgroundColor: '#0ba8bc'}}>
@@ -46,6 +40,12 @@ class Meal extends React.Component {
             pathname: `/addmood`,
             state: {meal: meal}}}>
             <button positive style={{height: '53px', width: '8rem', position: 'relative', alignSelf: 'center', backgroundColor: '#0ba8bc'}}>Add Mood to Last Meal</button>
+          </Link>
+          <Link to={{
+            pathname: `/editmeal`,
+            state: {meal: meal}}}>
+            <button positive style={{height: '53px', width: '8rem', position: 'relative', alignSelf: 'center', backgroundColor: '#0ba8bc'}}>
+                  Edit Meal</button>
           </Link>
         </div>
 
