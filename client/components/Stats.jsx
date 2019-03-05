1eefbb76b56dc92b76518fcd667ef37b493bedc2
Benjamin Peter Little <benlittle53@gmail.com>
2019-03-05 13:43:43 +1300

    Styling changes

--- client/components/Stats.jsx
+++ client/components/Stats.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import OverallStats from './OverallStats'
 import SelectedStats from './SelectedStats'
-
+import Grid from '@material-ui/core/Grid'
 
 class Stats extends React.Component {
   state = {
@@ -9,14 +9,23 @@ class Stats extends React.Component {
     selectedDates: null
   }
   render () {
-    console.log(this.state)
     return (
       <div>
-        <h1>Stats</h1>
-        <button onClick={() => this.setState({overall:true})}>Select overall mood</button>
-        <button onClick={() => this.setState({selectedDates:true})}>Select mood based on dates</button>
-        {this.state.overall && <OverallStats /> }
-        {this.state.selectedDates && <SelectedStats />}
+        <h1 style={{textAlgin:'center'}}>Mood meter</h1>
+        <Grid container alignContent="center" justify="center" >
+        
+        <div>
+          <button 
+              style={{display: 'inline-block', width:'200px', height: '30px',padding: '5px', margin:'20px'}}
+              onClick={() => this.setState({overall:true})}>Select overall mood</button>
+            <button 
+              style={{display: 'inline-block', width:'200px', height: '30px',padding: '5px', margin:'20px'}}
+              onClick={() => this.setState({selectedDates:true})}>Select mood based on dates</button>
+            {this.state.overall && <OverallStats /> || <SelectedStats />}
+        </div>
+          
+        </Grid>
+        
         
       </div>
     )
