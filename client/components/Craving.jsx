22f9f5043c0ebd402a0e2c7abc8d4df196d0848f
Ngapeita Leilua - Gotz <ngapeita@outlook.com>
2019-03-06 16:24:09 +1300

    created cravings component

---
+++ client/components/Craving.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Craving extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Craving
